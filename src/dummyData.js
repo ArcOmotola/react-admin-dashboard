42d1af49accf82d854b101ef2b57dbdd2822c06e
ArcOmotola <ogunsola40@gmail.com>
2021-10-18 14:06:35 +0100

    added chart component

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000,
+    },
+    {
+        name: 'Feb',
+        "Active User": 6000,
+    },
+    {
+        name: 'Mar',
+        "Active User": 7300,
+    },
+    {
+        name: 'Apr',
+        "Active User": 2700,
+    },
+    {
+        name: 'May',
+        "Active User": 2600,
+    },
+    {
+        name: 'Jun',
+        "Active User": 4000,
+    },
+    {
+        name: 'Jul',
+        "Active User": 7800,
+    },
+    {
+        name: 'Aug',
+        "Active User": 9200,
+    },
+    {
+        name: 'Sep',
+        "Active User": 5000,
+    },
+    {
+        name: 'Oct',
+        "Active User": 4000,
+    },
+    {
+        name: 'Nov',
+        "Active User": 6000,
+    },
+    {
+        name: 'Dec',
+        "Active User": 2000,
+    }, 
+];
